08a4b769bc40248582b81e313aadfc9567ec9c45
Tareq <tareqaldhaifani@gmail.com>
2025-09-10 15:01:29 +0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Tareq Aldhaifani on 10/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
